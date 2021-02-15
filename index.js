f3abb0b91da57aef30b25eea5472140c9e1db17f
M.H <basketball.mero63@gmail.com>
2021-02-15 01:51:11 +0000

    forheroku

--- index.js
+++ index.js
@@ -36,13 +36,6 @@ http.createServer(function(req,res){
     });
     break;
     
-  case '/team': //拡張子が/(index.html)だった場合はindex.htmlを返す
-    fs.readFile('team.html',function(err,data){
-      res.writeHead(200,{"Content-Type":"text/html"});
-      res.end(data,'utf-8');
-    });
-    break;
-    
   case '/user': //拡張子が/(index.html)だった場合はindex.htmlを返す
     fs.readFile('user.html',function(err,data){
       res.writeHead(200,{"Content-Type":"text/html"});
@@ -59,6 +52,216 @@ http.createServer(function(req,res){
     
   case '/user_other': //拡張子が/(index.html)だった場合はindex.htmlを返す
     fs.readFile('user_other.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=1':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+  
+  case '/team?number=2':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=3':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+  
+  case '/team?number=4':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=5':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+  
+  case '/team?number=6':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=7':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+  
+  case '/team?number=8':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=9':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=10':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=11':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=12':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=13':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=14':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=15':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=16':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=17':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=18':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=19':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=20':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=21':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=22':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=23':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=24':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=25':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=26':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=27':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=28':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=29':
+    fs.readFile('team.html',function(err,data){
+      res.writeHead(200,{"Content-Type":"text/html"});
+      res.end(data,'utf-8');
+    });
+    break;
+    
+  case '/team?number=30':
+    fs.readFile('team.html',function(err,data){
       res.writeHead(200,{"Content-Type":"text/html"});
       res.end(data,'utf-8');
     });
