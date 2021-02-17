79f030abb43e55dde89c9bf1169aac58f744b80d
M.H <basketball.mero63@gmail.com>
2021-02-17 23:53:12 +0000

    heroku

--- js/user_other.js
+++ js/user_other.js
@@ -9,6 +9,7 @@ new Vue({
     return {
       user: {},
       user_login: {},
+      user_login_followings: [],
       activefollow: '',
       count_favoritings: '',
       count_followers: '',
@@ -44,7 +45,8 @@ new Vue({
         .then(response => {
           console.log(response);
           this.user_login = response.data.user;
-          const arr = response.data.followings.map(function(user){
+          this.user_login_followings = response.data.followings;
+          /*const arr = response.data.followings.map(function(user){
             return user.id;
           });
           console.log(arr);
@@ -52,7 +54,7 @@ new Vue({
             this.activefollow = 'true';
           }else{
             this.activefollow = 'false';
-          }
+          }*/
         })
         .catch(error => {
           console.log(error);
@@ -63,7 +65,7 @@ new Vue({
       window.location.href = 'https://nba-view24.herokuapp.com/';
     }
   },
-  methods: {
+  /*methods: {
     follow: function(){
       axios
         .post('https://nba-api24.herokuapp.com/relationships/create', {
@@ -109,7 +111,128 @@ new Vue({
           return false;
         });
     }
-  },
+  },*/
   
 });
 
+
+
+
+
+
+
+Vue.component('follow-button', {
+  props:{
+    user_other_id:{
+      type: Number,
+      required: true,
+    },
+    user_login_id:{
+      type: Number,
+      required: true,
+    },
+    user_login_followings:{
+      type: Array,
+      required: true,
+    },
+    user_login_token:{
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return{
+      UserOtherId: this.other_user_id,
+      UserLoginId: this. user_login_id,
+      UserLoginFollwings: this.user_login_followings,
+      Token: this.user_login_token,
+    };
+  },
+  computed: {
+    followcompare(){
+      console.log(this.UserOtherId);
+      const UserOtherId = this.UserOtherId;
+      const arr = this.UserLoginFollwings.map(function(user){
+        return user.id;
+      });
+      return this.followsort(UserOtherId, arr);
+    }
+  },
+  methods:{
+    followsort: function(UserOtherId, arr){
+      console.log(UserOtherId);
+      console.log(arr);
+      if( arr.includes(UserOtherId)){
+        return false;
+      }else{
+        return true;
+      }
+    },
+    follow: function(){
+      axios
+        .post('https://nba-api24.herokuapp.com/relationships/create', {
+          follow_id: this.user.id 
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${this.user_login.token}`,
+          }
+        },
+        {
+          withCredentials: true,
+        },
+        )
+        .then(function (response) {
+          console.log(response);
+          this.getuser();
+        })
+        .catch(function (error) {
+          alert('フォローできませんでした。');
+          console.log(error);
+          return false;
+        });
+    },
+    unfollow: function(){
+      axios
+        .delete(`https://nba-api24.herokuapp.com/relationships/${this.user.id}`, {
+          headers: {
+            Authorization: `Bearer ${this.user_login.token}`,
+          }
+        },
+        {
+          withCredentials: true,
+        },
+        )
+        .then(function (response) {
+          console.log(response);
+          this.getuser();
+        })
+        .catch(function (error) {
+          alert('アンフォローできませんでした');
+          console.log(error);
+          return false;
+        });
+    },
+    getuser: function(){
+      var id = this.UserLoginId;
+      axios
+        .get(`https://nba-api24.herokuapp.com/users/${id}`)
+        .then(response => {
+          console.log(response.data);
+          this.UserLoginFollwings = response.data.followings;
+          /*ここにコンポーネントだけリロードできるようにする。*/
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  },
+  template:`
+    <section  v-if="followcompare">
+        <button type = "botton" class="btn btn-primary btn-lg" v-on:click="follow">follow</button>
+    </section>
+    <section v-else">
+        <button type = "botton" class="btn btn-danger btn-lg" v-on:click="unfollow">unfollow</button>
+    </section>
+    `,
+});
